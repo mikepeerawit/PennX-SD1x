938892c8d97353d930b25c815d7746d25b1afa79
Peerawit Chariyawongsiri <michael.chry@gmail.com>
2021-05-30 10:26:25 +0700

    Add files via upload

---
+++ Week 4-Battleship/EmptySea.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class EmptySea extends Ship{
+    /*
+	 * This constructor sets the inherited length variable to 1.
+	 */
+	public EmptySea() {
+		this.setLength(1);
+		this.setHit(new boolean[1]);
+		Arrays.fill(this.getHit(), false);
+	}
+	
+	@Override
+	public String getShipType() {
+		return "empty";
+	}
+	
+	/*
+	 * This method overrides shootAt(int row, int column) that is inherited from Ship, 
+	 * and always returns false to indicate that nothing was hit.
+	 */
+	@Override
+	public boolean shootAt(int row, int column) {
+		this.getHit()[0] = true;
+		return false;
+	}
+	
+	/*
+	 * always returns false to indicate that you didn’t sink anything.
+	 */
+	@Override
+	public boolean isSunk() {
+		return false;
+	}
+	
+	/*
+	 * Returns a single-character String to use in the Ocean’s print method.
+	 */
+	@Override
+	public String toString() {
+		return this.getHit()[0] == true ? "-" : ".";
+	}
+}
